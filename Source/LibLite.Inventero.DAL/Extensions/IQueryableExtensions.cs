443d8c4ded57efb9fb4b69194f0543e74e85e29c
dgrajewski <dawids115@gmail.com>
2023-01-18 21:58:36 +0100

    Added searching

--- Source/LibLite.Inventero.DAL/Extensions/IQueryableExtensions.cs
+++ Source/LibLite.Inventero.DAL/Extensions/IQueryableExtensions.cs
@@ -1,50 +1,53 @@
 ﻿using LibLite.Inventero.Core.Consts;
+using System.Linq.Dynamic.Core;
 using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
 
 namespace LibLite.Inventero.DAL.Extensions
 {
     internal static class IQueryableExtensions
     {
         public static IOrderedQueryable<T> OrderBy<T>(
-            this IQueryable<T> source,
+            this IQueryable<T> query,
             string property,
             string direction)
         {
             return direction == SortDirection.ASC
-                ? source.OrderBy(property)
-                : source.OrderByDescending(property);
+                ? query.OrderBy(property)
+                : query.OrderByDescending(property);
         }
 
         public static IOrderedQueryable<T> OrderBy<T>(
-            this IQueryable<T> source,
+            this IQueryable<T> query,
             string property)
         {
-            return ApplyOrder(source, property, "OrderBy");
+            return ApplyOrder(query, property, "OrderBy");
         }
 
         public static IOrderedQueryable<T> OrderByDescending<T>(
-            this IQueryable<T> source,
+            this IQueryable<T> query,
             string property)
         {
-            return ApplyOrder(source, property, "OrderByDescending");
+            return ApplyOrder(query, property, "OrderByDescending");
         }
 
         public static IOrderedQueryable<T> ThenBy<T>(
-            this IOrderedQueryable<T> source,
+            this IOrderedQueryable<T> query,
             string property)
         {
-            return ApplyOrder(source, property, "ThenBy");
+            return ApplyOrder(query, property, "ThenBy");
         }
 
         public static IOrderedQueryable<T> ThenByDescending<T>(
-            this IOrderedQueryable<T> source,
+            this IOrderedQueryable<T> query,
             string property)
         {
-            return ApplyOrder(source, property, "ThenByDescending");
+            return ApplyOrder(query, property, "ThenByDescending");
         }
 
         private static IOrderedQueryable<T> ApplyOrder<T>(
-            IQueryable<T> source,
+            IQueryable<T> query,
             string property,
             string methodName)
         {
@@ -67,8 +70,35 @@ namespace LibLite.Inventero.DAL.Extensions
                             && method.GetGenericArguments().Length == 2
                             && method.GetParameters().Length == 2)
                     .MakeGenericMethod(typeof(T), type)
-                    .Invoke(null, new object[] { source, lambda });
+                    .Invoke(null, new object[] { query, lambda });
             return (IOrderedQueryable<T>)result;
+        }
+
+        public static IQueryable<T> SearchBy<T>(this IQueryable<T> query, string searchBy)
+        {
+            if (string.IsNullOrWhiteSpace(searchBy)) { return query; }
+            var searchQuery = CreateSearchQuery<T>();
+            return query.Where(searchQuery, searchBy.ToLower());
+        }
+
+        private static string CreateSearchQuery<T>()
+        {
+            var properties = GetProperties<T>();
+            var stringBuilder = new StringBuilder();
+            foreach (var property in properties)
+            {
+                stringBuilder.Append($"string(object({property.Name})).ToLower().Contains(@0) OR ");
+            }
+            var result = stringBuilder.ToString();
+            return result[..result.LastIndexOf("OR ")];
+        }
+
+        private static IEnumerable<PropertyInfo> GetProperties<T>()
+        {
+            return typeof(T)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(p => p.PropertyType == typeof(string))
+                .ToList();
         }
     }
 }
